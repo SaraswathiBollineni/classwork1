8406a4a30482612039ee5f9c9bf1fa726784fe67
SaraswathiBollineni <113283829+SaraswathiBollineni@users.noreply.github.com>
2022-09-30 09:49:39 +0530

    Add files via upload

---
+++ bookdemo.java
@@ -0,0 +1,12 @@
+
+public class Exp4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book a[]=new Book[30];
+		a[1]=new Book("Java the complete reference",234567,"Herbert Schildt","Ram");
+		a[1].getBookInfo();
+	}
+
+}
+
